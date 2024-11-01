1dba152279c92b1e2cbde88a1168ecdd103a3632
AlexChequer <alexchequer05@gmail.com>
2024-11-01 12:07:00 -0300

    back de predios + aquarios feito

--- app.py
+++ app.py
@@ -57,8 +57,69 @@ def login():
         return jsonify({'msg': 'Credenciais inválidas'}), 401
 
 
+# PREDIOS
+@app.route('/predios', methods=['GET'])
+def listar_predios():
+    documentos = mongo.db.aquarios.find({}, {'predio': 1})
+    predios = [doc['predio'] for doc in documentos]
+    return jsonify({'predios': predios}), 200
+
 # AQUARIOS
 
+@app.route('/predios/<predio>', methods=['GET'])
+def listar_aquarios_por_predio(predio):
+    documento = mongo.db.aquarios.find_one({'predio': predio.upper()})
+    if not documento:
+        return jsonify({'msg': 'Prédio não encontrado'}), 404
+    
+    resultado = []
+    for andar in sorted(documento['andares'], key=lambda x: x['andar']):
+        aquarios_info = {
+            'andar': andar['andar'],
+            'aquarios': andar['aquarios']
+        }
+        resultado.append(aquarios_info)
+    return jsonify(resultado), 200
+
+# AQUARIO OCUPAR/DESOCUPAR
+@app.route('/aquarios/ocupar/<predio>/<int:andar>/<int:numero>', methods=['PUT'])
+def ocupar_aquario(predio, andar, numero):
+    predio = predio.upper()
+    
+    # Atualiza o campo 'ocupado' do aquário especificado para True
+    result = mongo.db.aquarios.update_one(
+        {'predio': predio},
+        {'$set': {'andares.$[andarElem].aquarios.$[aquarioElem].ocupado': True}},
+        array_filters=[
+            {'andarElem.andar': andar},
+            {'aquarioElem.numero': numero, 'aquarioElem.ocupado': False}
+        ]
+    )
+    
+    if result.modified_count == 0:
+        return jsonify({'msg': 'Aquário não encontrado ou já está ocupado'}), 400
+    
+    return jsonify({'msg': 'Aquário ocupado com sucesso'}), 200
+
+
+@app.route('/aquarios/desocupar/<predio>/<int:andar>/<int:numero>', methods=['PUT'])
+def desocupar_aquario(predio, andar, numero):
+    predio = predio.upper()
+    
+    # Atualiza o campo 'ocupado' do aquário especificado para False
+    result = mongo.db.aquarios.update_one(
+        {'predio': predio},
+        {'$set': {'andares.$[andarElem].aquarios.$[aquarioElem].ocupado': False}},
+        array_filters=[
+            {'andarElem.andar': andar},
+            {'aquarioElem.numero': numero, 'aquarioElem.ocupado': True}
+        ]
+    )
+    
+    if result.modified_count == 0:
+        return jsonify({'msg': 'Aquário não encontrado ou já está desocupado'}), 400
+    
+    return jsonify({'msg': 'Aquário desocupado com sucesso'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
